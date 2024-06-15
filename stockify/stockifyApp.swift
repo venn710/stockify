b6ed40149575d1fbcefc18cd8892a528b046ede5
venn <vennboddula@gmail.com>
2024-06-15 23:31:30 +0530

    Initial Commit

---
+++ stockify/stockifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  stockifyApp.swift
+//  stockify
+//
+//  Created by Venkatesham Boddula on 15/06/24.
+//
+
+import SwiftUI
+
+@main
+struct stockifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
